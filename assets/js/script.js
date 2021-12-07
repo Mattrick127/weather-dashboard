03b214996e2dfdf9b6e273d2746d0275fe45d0db
Sodaman <mattrick2214@gmail.com>
2021-12-07 17:53:30 -0600

    added buttons for nearby cities and successfully looked at weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-var getWeatherReport
+var getWeatherReport = function() {
+    console.log("function was called");
+    fetch ("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=44fd4a683d34b7393e0bfa504d69c463");
+};
+
+getWeatherReport();
