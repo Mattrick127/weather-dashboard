cfdc35464dcb577b84347fadbfda1539ac447f5b
Sodaman <mattrick2214@gmail.com>
2021-12-08 01:54:17 -0600

    figured how to change weather to give back temperature in Farenheit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var getWeatherReport = function(city) {
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=44fd4a683d34b7393e0bfa504d69c463";
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=44fd4a683d34b7393e0bfa504d69c463";
 
     
     console.log("function was called")
@@ -10,4 +10,4 @@ var getWeatherReport = function(city) {
     })
 };
 
-getWeatherReport("London");
+getWeatherReport("San Antonio");
